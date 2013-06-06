025a6235ab330f4f0173a4e55cb38771b389bf52
Steven Zhu <steven.zhu@zionsoft.net>
2013-06-06 13:19:24 +0300

    Added webpage to show all available translations.

--- src/bible/init.go
+++ src/bible/init.go
@@ -8,6 +8,6 @@ func init() {
     http.Handle("/1.0/downloadTranslation", appHandler(downloadTranslationHandler))
     http.Handle("/1.0/translations", appHandler(queryTranslationsHandler))
 
-    http.Handle("/admin/view/uploadTranslation", appHandler(uploadTranslationViewHandler))
+    http.Handle("/admin/uploadTranslationView", appHandler(uploadTranslationViewHandler))
     http.Handle("/admin/uploadTranslation", appHandler(uploadTranslationHandler))
 }
